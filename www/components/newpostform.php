8e2feaf45addb0bce1dd308ac9c0307fbd122cc5
carowood <carowood@gmail.com>
2017-11-12 21:45:03 +0100

    www/components/newpostform.php and www/parts/savepost.php updated to include dropdown and check post info is entered correctly by user

--- www/components/newpostform.php
+++ www/components/newpostform.php
@@ -4,12 +4,23 @@ Form
     <input type="text" name ="title" placeholder="Skriv din titel här"> 
     <textarea name="text" rows="10" cols="30"></textarea>
     
-    
+   <!--
     <input type = "checkbox" name="categoryid" value ="1">Solglasögon
     <input type = "checkbox" name="categoryid" value ="2">Klockor
     <input type = "checkbox" name="categoryid" value ="3">Inredning
     <input type = "checkbox" name="categoryid" value ="4">Lifestyle
     <input type = "checkbox" name="categoryid" value ="5">Övrigt
+    -->
+    
+    <!--added drop-down instead of checkbox as user can only choose one category at a time at the moment-->
+    <select name="categoryid">
+      <option selected="" >Välja en kategori</option>    
+      <option value="1" >Solglasögon</option>
+      <option value="2" >Klockor</option>
+      <option value="3" >Inredning</option>
+      <option value="4">Lifestyle</option>
+      <option value="5" >Övrigt</option>
+    </select>
      
     
     <input type="submit" value="Publicera"> <!--'Publicera' matches wireframe-->

c21bca405e4689eb7b886cb40854064fbd12cde2
Emma Uddesson <emma.uddesson@gmail.com>
2017-11-13 17:01:35 +0100

    Fixes issue with footer not showing when logging in and adds </br> in form

--- www/pages/loginform.php
+++ www/pages/loginform.php
@@ -16,13 +16,16 @@
 
     
 ?>
-   
-<form action="/millhouseblog/www/parts/login.php" method="post">
-    <input type="username" name ="username" placeholder="Användarnamn"> 
-    <input type="password" name ="password" placeholder="Lösenord">
-    <input type="submit" value="Logga in">
-</form>
-    
-<a href="/millhouseblog/www/?page=register">Registrera ny användare</a>
+<div class="login_form_container">   
+    <form action="/millhouseblog/www/parts/login.php" method="post">
+        <input type="username" name ="username" placeholder="Användarnamn"></br>
+        <input type="password" name ="password" placeholder="Lösenord"></br>
+        <input type="submit" value="Logga in">
+    </form>
+        
+    <a href="/millhouseblog/www/?page=register">Registrera ny användare</a>
+</div>
 
 <!-------- LOGIN-FORM ENDS ---------->
+
+<?php include 'components/footer.php'; ?>

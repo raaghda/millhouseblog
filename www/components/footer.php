97b3ad062048f2f1b2ef20746e90279669384e12
Emma Uddesson <emma.uddesson@gmail.com>
2017-11-13 15:29:00 +0100

    Added footer (content + styling) and font awesome-script

--- www/components/footer.php
+++ www/components/footer.php
@@ -1,11 +1,22 @@
 </main>
 
 <footer>
+    <ul>
+      <li>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><i class="fa fa-facebook-square"></i></a>
+        <a href="#"><i class="fa fa-linkedin-square"></i></a>
+      </li>
+      <li><a href="https://github.com/raaghda/millhouseblog">info@millhouse.se</a></li>
+      <li><a href="https://github.com/raaghda/millhouseblog">millhouse.se</a></li>
+    </ul>
 </footer>
 <!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- javascript-->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- fontawesome-->
+<script src="https://use.fontawesome.com/67b80e2b65.js"></script>
 <script src="javascript.js"></script> 
 </body>
 </html>

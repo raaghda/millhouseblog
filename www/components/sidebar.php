12f8496bb98201a4ad6719c6b9f94e5912ec4aed
cami <evacamilla.s@gmail.com>
2017-11-14 17:23:41 +0100

    deleted text from sidebar

--- www/components/sidebar.php
+++ www/components/sidebar.php
@@ -1,10 +1,4 @@
 <aside class="">
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
 <? require 'pages/categories.php'; ?>
->>>>>>> 54197ca64dfbd9c26d0926871fc24d2f79765c8b
->>>>>>> a473844ff27cc9806104c2986bfd21b93813683e
 FETCH AND LOOP OUT 5 LATEST COMMENT-LINKS IN NAV
 </aside>

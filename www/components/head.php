d4b843da68229b9ab0b9a26b3b3135d5c221b264
patrik909 <patrik.eriksson@medieinstitutet.se>
2017-11-15 12:27:03 +0100

    added dynamic title, not all pages yet

--- www/components/head.php
+++ www/components/head.php
@@ -4,7 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>
+       <?
+        
+        $pagename = $_GET ['page'];
+        
+        if($pagename == "loginform"){
+            echo 'Logga in';
+        } else if($pagename == "register"){
+            echo 'Registrera dig'; 
+        } else if($pagename == "home"){
+            echo 'Hem'; 
+        } else if($pagename == "categories"){
+            echo 'Kategorier';
+        } else if($pagename == "faq"){
+            echo 'FAQ';
+        } else if($pagename == "profile"){
+            echo 'Min profil';
+        }  
+        
+        ?>
+    </title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" type="text/css" >

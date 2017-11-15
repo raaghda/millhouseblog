e03a09dd408f14ffc96c261dd9cb025e83bfd6ab
cami <evacamilla.s@gmail.com>
2017-11-15 21:57:14 +0100

    Added functions and linked to functions.php. Took away the comments.

--- www/pages/home.php
+++ www/pages/home.php
@@ -1,4 +1,7 @@
 <?php
+require 'parts/database.php';
+require 'parts/functions.php';
+
         /*------     IF ISSET = IF USER IS LOGGED IN     ------*/
 
         if(isset($_SESSION["user"])){
@@ -19,74 +22,43 @@
             if(isset($_GET['expired'])){
                 echo $_GET['expired'];
             }
-        
         }
-    
-//FETCH POSTS FROM 
-//PUT THIS IN PARTS...? AS FETCH_POST T.EX..
-require 'parts/database.php';
+
+//FETCH POSTS
+//MAYBE PUT THIS IN PARTS...? AS FETCH_POST T.EX..
 $statement = $pdo->prepare("SELECT * FROM post ORDER by date DESC");
-  $statement->execute();
-  $post = $statement->fetchAll(PDO::FETCH_ASSOC);
-  $keys = array_keys($post);
+$statement->execute();
+$post = $statement->fetchAll(PDO::FETCH_ASSOC);
+$keys = array_keys($post);
 
-//LOOPING OUT THE POSTS THROUGH $post
-//en liten detalj: hur ska man göra ifall det endast skulle finnas mindre än 5 inlägg inte skrivs ut felmeddelande "unknown offset..."
-  for($i=0; $i<5; $i++){
-      $user_id = $post[$keys[$i]]['userid'];
-      $post_id = $post[$keys[$i]]['postid'];
-            //ska göras till funktion
-            //hämta ut username FROM user där $userid == $userid och lagra i $username.
-            //GÖRA FUNKTION
-            $statement = $pdo->prepare("SELECT username FROM user WHERE userid = '$user_id'");
-            $statement->execute();
-            $user_info = $statement->fetch(PDO::FETCH_ASSOC);
-            $username = $user_info['username'];
+ for($i=0; $i<5; $i++){
+    $user_id = $post[$keys[$i]]['userid'];
+    $post_id = $post[$keys[$i]]['postid'];
+    $category_id = $post[$keys[$i]]['categoryid'];
 
-            //hämta ut comments som har detta post_id genom INNER JOIN
-            //lagra i array och loopa ut nedanför post
-            $statement = $pdo->prepare("SELECT * FROM comment INNER JOIN post ON comment.postid = post.postid WHERE comment.postid = $post_id");
-            $statement->execute();
-            $comments = $statement->fetchAll(PDO::FETCH_ASSOC);
-            $number_of_comments = count($comments);
-      ?>
-      
+    $category_name = get_row_with_input('name', 'category', 'categoryid', $category_id);
+    $username = get_row_with_input('username', 'user', 'userid', $user_id);
+
+    $number_of_comments = count_comments($post_id);
+
+
+    //LOOPING OUT THE POSTS
+    ?>  
       <article class="">
       <header class=””>
           <!--<meta>kategorierna som meta???-->
           <h2 class=””><?=$post[$keys[$i]]['title'];?></h2>
           <time class=""><?=$post[$keys[$i]]['date'];?></time> 
-          <span>Categories</span>
+          <span class=""><?=$category_name?></span>
           <span class=""><?= $number_of_comments ?></span> 
           <span class=""><?= $username?></span>
       </header>
       <p><?=$post[$keys[$i]]['text'];?></p>
 
-        <nav class=””><a href="/millhouseblog/www/?page=viewpost&id=<?= $post_id ?>">Läs hela inlägget...</a>
+        <nav class=””><a href="/millhouseblog/www/?page=viewpost&id=<?= $post_id ?>">Läs hela inlägget..</a>
           <a href="/millhouseblog/www/?page=viewpost&id=<?= $post_id ?>">Kommentera</a>
-        </nav>
-
-      <?php
-        //looping out comments
-        foreach($comments as $comment_info){
-            $user_id = $comment_info['userid'];
-            //göra funktion
-            $statement = $pdo->prepare("SELECT username FROM user WHERE userid = '$user_id'");
-            $statement->execute();
-            $userinfo = $statement->fetch(PDO::FETCH_ASSOC);
-            $username = $user_info['username'];
-            ?>
-          <article class=””> 
-            <header class="">
-                <time class=""><?=$comment_info['date']?></time> 
-                <span>av <?=$username?></span>
-            </header>
-            <p class=""><?=$comment_info["comment"]?></p>    
-            <?php
-            }
-            ?>
-           </article><!--/comment article-->    
-  </article><!--/blogpost article-->
+        </nav>   
+  </article><!--/post article-->
   --------------< hr >--------------
   <?php } 
   
@@ -94,7 +66,7 @@ $statement = $pdo->prepare("SELECT * FROM post ORDER by date DESC");
   ?>
 
 
-<!--Blogpost skeleton to be looped out
+<!--Blogpost skeleton
 <article class="">
 <header class=””>
 <meta kategorierna som meta???

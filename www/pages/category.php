28f1ff0a8f672dcf4040d59564a23552fc5c6384
carowood <carowood@gmail.com>
2017-11-16 16:32:02 +0100

    fixed undefined index

--- www/pages/category.php
+++ www/pages/category.php
@@ -4,7 +4,7 @@
 
 <?
 
-$categoryid = $_GET["categoryid"];
+$categoryid = isset($_GET["categoryid"]);
 
     $statement = $pdo->prepare(
         "SELECT userid, title, date, text, category.name as category_name FROM post INNER JOIN category ON post.categoryid = category.categoryid WHERE post.categoryid = :categoryid" 

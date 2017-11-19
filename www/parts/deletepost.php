8efb550f84b57dd9cf6251a7a8641a37894e4dee
carowood <carowood@gmail.com>
2017-11-19 20:56:13 +0100

    delete comments query added to delete.php part

--- www/parts/deletepost.php
+++ www/parts/deletepost.php
@@ -10,9 +10,17 @@ if (!($post_id)){
     
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
+
+$statement = $pdo->prepare("DELETE FROM comment WHERE postid = :postid");
+$statement->execute(array(":postid" => $post_id,));
+    
 //query created to delete the task from the database, which the user has chosen via the form in fetch_all_todos.php   
 $statement = $pdo->prepare("DELETE FROM post WHERE postid = :postid");
 $statement->execute(array(":postid" => $post_id,));
+    
+
+    
+
 
 //Should the user be redirected to the homepage?
 header ("Location: /millhouseblog/www/?page=home");

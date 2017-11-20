222ccd079b66b5653a4a23de753126c6384f0a7c
carowood <carowood@gmail.com>
2017-11-20 20:34:39 +0100

    edit post functionality

--- www/pages/editpost.php
+++ www/pages/editpost.php
@@ -1,55 +1,80 @@
 <?php
-require '../parts/database.php';
+require './parts/database.php';
 
-    $statement = $pdo->prepare(
-        "SELECT * FROM category" 
-    );
+$post_id = $_POST["post_id"]; //Post sent from edit button input on viewpost.php
 
-    $statement->execute();
+$statement = $pdo->prepare("SELECT * FROM post WHERE postid = $post_id");
 
-    $categories = $statement->fetchAll(PDO::FETCH_ASSOC);
+//$statement = $pdo->prepare("SELECT * FROM post WHERE postid = :postid");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post){    
+$title = $post['title'];
+$user_id = $post['userid'];
+$post_id = $post['postid'];
+$category_id = $post['categoryid'];
+$image = $post['image'];
+$date_of_post = $post['date'];
+$text = $post['text'];
+
+
+
+$statement = $pdo->prepare("SELECT * FROM category");
+$statement->execute();
+$categories = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
 <div class="container createpost">
-    <h2>Skriv nytt inlägg</h2>
-    <form action="parts/update.php" method="POST" enctype="multipart/form-data">
+    <h2>Redigera ditt inlägg</h2>
+    <form action="../www/parts/updatepost.php" method="POST" enctype="multipart/form-data">
+        <input type="hidden" name="postid" value="<?=$post_id;?>">
 
         <div class="container form">
             <div class="form-group row">
                 <div class="col-sm-12">
-                    <input required id="title" type="text" name="title" placeholder="<?=$_POST["title"];?>">
+                    <input required id="title" type="text" name="title" value="<?=$title;?>" placeholder="">
                 </div>
             </div>
             <div class="form-group row">
                 <div class="col-sm-12">
-                    <textarea required name="text" rows="10" cols="30" placeholder="<?=$_POST["text"];?>"></textarea>
+                    <textarea required name="text" rows="10" cols="30" placeholder=""><?=$text;?></textarea>
                 </div>
             </div>
 
             <div class="form-group row">
                 <div class="col-sm-3">
+                   <!--input type="text" name="categoryid" value="<?=$category_name;?>"-->
                     <select required name="categoryid"> 
-                        <option value="" >Välja Kategori</option>  
+                        <!--option value="" >Välja Kategori</option-->  
       
                           <?php
                             foreach ($categories as $category){
+                                
+                                //if post-table categoryid is the same as the category-dropdown
+                                if ($category_id == $category['categoryid']){
+                                    
                             ?>
-                   
-                        <option value="<?=$category['categoryid'];?>"><?=$category['name'];?></option> 
+                        <!--Select the option dropdown that corresponds with the post's original category-->   
+                        <option selected value="<?=$category['categoryid'];?>"><?=$category['name'];?></option> 
                
                           <?php
-                            }
+                            }else{
+                                ?>
+                            <option value="<?=$category['categoryid'];?>"><?=$category['name'];?></option> 
+                            <?php   
+                            }}
                             ?>
                     </select>
                 </div>
             </div>
 
-            <div class="form-group row">
+            <!--div class="form-group row">
                 <div class="col-sm-10">
                     <input type="file" name="file" accept=".jpg, .jpeg, .png, .gif"><br><br>
                 </div>
-            </div>
+            </div-->
         </div>
 
         <div class="form-group row">
@@ -59,3 +84,6 @@ require '../parts/database.php';
         </div>
     </form>
 </div>
+<?php
+  }
+?>

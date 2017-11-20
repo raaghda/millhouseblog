7818ffd77f491c40bf09e1d67a75145a264c72b4
cami <evacamilla.s@gmail.com>
2017-11-20 10:55:32 +0100

    added html unordered list to sidebar

--- www/components/sidebar.php
+++ www/components/sidebar.php
@@ -1,16 +1,21 @@
 <aside class="">
-
-<a href="/millhouseblog/www/?page=category&categoryid=1">
-    Solglasögon
-</a>
-<a href="/millhouseblog/www/?page=category&categoryid=2">
-    Klockor
-</a>
-<a href="/millhouseblog/www/?page=category&categoryid=3">
-    Inredning
-</a>
-<a href="/millhouseblog/www/?page=category&categoryid=4">
-    Övrigt
-</a>
+<ul>
+    <li>
+        <a href="/millhouseblog/www/?page=category&categoryid=1">
+        Solglasögon</a>
+    </li>
+    <li>
+        <a href="/millhouseblog/www/?page=category&categoryid=2">
+            Klockor</a>
+    </li>
+    <li>
+        <a href="/millhouseblog/www/?page=category&categoryid=3">
+            Inredning</a>
+    </li>
+    <li>
+        <a href="/millhouseblog/www/?page=category&categoryid=4">
+            Övrigt</a>
+    </li>
+</ul>
 FETCH AND LOOP OUT 5 LATEST COMMENT-LINKS IN NAV
 </aside>

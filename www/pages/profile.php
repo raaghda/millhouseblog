933451de2d3d79e8436a10060d36d30efc8e39b7
Emma Uddesson <emma.uddesson@gmail.com>
2017-11-20 14:57:02 +0100

    Further edits on displaying comments

--- www/pages/profile.php
+++ www/pages/profile.php
@@ -44,11 +44,11 @@ $fetched_user = $statement->fetch(PDO::FETCH_ASSOC);
 
 <div class="container profile_content">
     <div class="row">
-    <div class="col-12 col-lg-8 offset-lg-2">    
-            <h1 id=my_latest_posts>Mina senaste inlägg:</h1>
+        <div class="col-12 col-lg-8 offset-lg-2">    
+            <h1 id=my_latest_posts>Senaste inläggen:</h1>
         </div>
     </div>
-     
+    
     <?php
     //Fetches posts made by logged in user, using the same display posts-strucutre as in home
     $statement = $pdo->prepare("SELECT * FROM post WHERE userid = {$userid} ORDER by date DESC");
@@ -89,7 +89,7 @@ $fetched_user = $statement->fetch(PDO::FETCH_ASSOC);
                 <a href="/millhouseblog/www/?page=viewpost&id=<?= $post_id ?>">Läs hela inlägget</a>
                 <a href="#">Redigera inlägg</a>
                 
-                <!-- DELETE IS NOT WORKING -->
+                <!-- DELETE IS CURRENTLY NOT WORKING -->
                 <form action="../www/parts/deletepost.php" method="POST">
                     <input type="hidden" name="post_id" value="<?= $post_info['postid'];?>">
                     <input type="submit" name="delete" value="Delete">   
@@ -99,48 +99,55 @@ $fetched_user = $statement->fetch(PDO::FETCH_ASSOC);
         </div> <!-- Closing row for each post-->
     </div> <!-- Closing col for each post -->
     <?php } endfor; ?>
-</div> <!-- Closing container profile content -->
-
-
-<h2>Mina senaste kommentarer:</h2>
-<?php
-$statement = $pdo->prepare("SELECT * FROM comment WHERE userid = {$userid} ORDER by date DESC");
-$statement->execute();
-$comments = $statement->fetchAll(PDO::FETCH_ASSOC);
-$keys = array_keys($comments);
-
-for ($i = 0; $i < 5; $i++):
-    //store post_id to get post_title
-    //store post_id to be able to link to that specific post
-    //user_id to getuser_name from user table
-    $post_id = $comments[$keys[$i]]['postid'];
-    $user_id = $comments[$keys[$i]]['userid'];
-    $comment = $comments[$keys][$i]['comment'];
-
-    //use the stored variables to get info from each table
-    //FUNCTIONS is in function.php
-    $post_title = get_row_with_input("title", "post", "postid", $post_id);
-    $username = get_row_with_input("username", "user", "userid", $user_id);
 
     
- 
-        //Loop through and display latest comments (max 5)
-    ?>  
+
     <div class="row">
-        <div class="col-12 col-lg-8 offset-lg-2">    
-            <article class="post">
-                <header>  
-                <span class="uppercase grey"><?=$category_name?></span>
-                <h2 class=”postheading”><?=$post[$keys[$i]]['title'];?></h2>
-                <div class=comment_box>
-                <p>Din kommentar:</p>
-                <p><?=$comments[$keys[$i]]['comment'];?></p>
-                </div>
-                </header>
-            </article>
-        </div> <!-- Closing row for each post-->
-    </div> <!-- Closing col for each post -->
-    
-    <?php endfor; ?>
+        <div class="user_comments_wrapper col-12 col-lg-8 offset-lg-2">    
+            <h2>Dina senaste kommentarer:</h2>
+        </div>
+    </div>
+
+    <?php
+    $statement = $pdo->prepare("SELECT * FROM comment WHERE userid = {$userid} ORDER by date DESC");
+    $statement->execute();
+    $comments = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $keys = array_keys($comments);
+
+    for ($i = 0; $i < 5; $i++):
+        $post_id = $comments[$keys[$i]]['postid'];
+        $user_id = $comments[$keys[$i]]['userid'];
+        $comment_date = $comments[$keys[$i]]['date'];
+        $comment_id = $comments[$keys[$i]]['commentid'];
+        $comment = $comments[$keys][$i]['comment'];
+
+        $post_title = get_row_with_input("title", "post", "postid", $post_id);
+        $username = get_row_with_input("username", "user", "userid", $user_id);
+
+        if($comment_id == NULL)
+        {
+            //Same fix as posts - fixes problem with "empty comments" showing if there are less than five
+            break; 
+        }
+        else
+        { 
+        ?>  
+        <div class="row">
+            <div class="col-12 col-lg-8 offset-lg-2">    
+                <article class="comment_box">
+                    
+                    <span class="uppercase grey"><?=$category_name?></span>
+                    <h3><?=$post[$keys[$i]]['title'];?></h3>
+                    
+                    <p>Din kommentar: 
+                    <?=$comments[$keys[$i]]['comment'];?></p>
+                    <time class="grey">Kommenterades den: <?=$comments[$keys[$i]]['date']?></time>
+                    <a href="/millhouseblog/www/?page=viewpost&id=<?= $post_id ?>">Läs hela inlägget</a>
+                    
+                </article>
+            </div> <!-- Closing row for each comment-->
+        </div> <!-- Closing col for each post -->
+        
+        <?php } endfor; ?>
 </div> <!-- Closing container profile content -->
     

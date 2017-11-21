78645534ea6f473b914c7fdd56393e3d950b9437
patrik909 <patrik.eriksson@medieinstitutet.se>
2017-11-21 15:32:11 +0100

    started working on the 404 page

--- www/pages/404.php
+++ www/pages/404.php
@@ -1,1 +1,16 @@
-ERR000000000000RRR
+<div class="container landingpage">
+
+
+
+<div class="page404">
+
+<h3>Sorry, this page is not<br>what you are looking for :(</h3>
+
+<img src="/millhouseblog/www/images/IndelibleAliveAmericancrow-max-1mb.gif">
+
+<h4>Please head back to <a href="/millhouseblog/www/?page=home">Home</a></h4>
+
+</div>
+
+</div>
+

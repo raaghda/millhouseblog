2bf81a51e223965ebad0c8dbdd50c668e9c279d1
Emma Uddesson <emma.uddesson@gmail.com>
2017-11-21 09:27:04 +0100

    First draft for FAQ-page

--- www/pages/faq.php
+++ www/pages/faq.php
@@ -1,1 +1,12 @@
-FAQ GOES HERE
+<div class="container-fluid faq">
+    <div class="row">
+        <div class="faq_box col-6 offset-3">
+            <h1>Om Millhouse</h1>    
+            <p>Blogportalen för dig som vill utforska din egen- och andras personliga stil.</br>
+            Här kan du inspireras av det senaste modet inom bland annat klockor, solglasögon och inredning.</br>
+            Skapa en egen profil för att själv vara med och skapa modet.</br>
+            Som medlem kan du skapa inlägg och bli en egen inspirationskälla – och dela den med andra.</br>
+            Milhouse Blogportal ägs av Millhouse AB.</p>
+        </div>
+    </div>
+</div>

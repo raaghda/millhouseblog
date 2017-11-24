ea26da785b0e70b0b15678e1e0756b863efba2ab
cami <evacamilla.s@gmail.com>
2017-11-24 11:16:13 +0100

    added comments

--- www/parts/functions.php
+++ www/parts/functions.php
@@ -1,21 +1,31 @@
   <?php
   require 'parts/database.php';
 
-function get_row_with_input($row_name, $table_name, $compare_row, $input){
+  
+  //get a specific value from a row in a table using an id to compare with the id's in the table
+  //You have a userid. You want to get the persons username. 
+  //so you compare the userid with all the userid's in the user table.
+  //Select username from user where userid = userid. Where $input is the userid you have to compare.
+function get_row_with_input($row_name, $table_name, $compare_row_name, $input){
     require 'parts/database.php';
-    $statement = $pdo->prepare("SELECT $row_name FROM $table_name WHERE $compare_row = $input");
-    $statement->execute();
+    $statement = $pdo->prepare("SELECT $row_name FROM $table_name WHERE $compare_row_name = $input");
+    $statement->execute();  
+    //Store it in an array.
     $result = $statement->fetch(PDO::FETCH_ASSOC);
+    //get the row_name from the array. Fex username
     return $result["$row_name"];
 }         
 
+
+//get all comments made on a specific post using post_id
 function count_comments($post_id){
     require 'parts/database.php';
     $statement = $pdo->prepare("SELECT * FROM comment INNER JOIN post ON comment.postid = post.postid WHERE comment.postid = $post_id");
     $statement->execute();
+    //store it in an array
     $comments = $statement->fetchAll(PDO::FETCH_ASSOC);
-    $number_of_comments = count($comments);
-    return $number_of_comments;
+    //counting the number of objects in the array using count function = the number of comments
+    return count($comments);
 }
 
 //takes in the string you want to shorten and the max-length you want

95ec173e2a6339613d225fc3e3fefc4250ba3d63
Emma Uddesson <emma.uddesson@gmail.com>
2017-11-26 19:00:58 +0100

    Trying small caps for footer

--- www/components/footer.php
+++ www/components/footer.php
@@ -7,8 +7,10 @@
         <a href="#"><i class="fa fa-facebook-square"></i></a>
         <a href="#"><i class="fa fa-linkedin-square"></i></a>
       </li>
+      <span class="smallcaps">
       <li><a href="https://github.com/raaghda/millhouseblog">info@millhouse.se</a></li>
       <li><a href="https://github.com/raaghda/millhouseblog">millhouse.se</a></li>
+      </span>
     </ul>
 </footer>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>

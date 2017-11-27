ed94b87c0d24be3917dd6d6f10c1eda98912e860
patrik909 <patrik.eriksson@medieinstitutet.se>
2017-11-27 11:39:32 +0100

    more cleaning

--- www/components/footer.php
+++ www/components/footer.php
@@ -1,23 +1,27 @@
-</main>
+        </main>
 
-<footer>
-    <ul>
-      <li>
-        <a href="#"><i class="fa fa-instagram"></i></a>
-        <a href="#"><i class="fa fa-facebook-square"></i></a>
-        <a href="#"><i class="fa fa-linkedin-square"></i></a>
-      </li>
-      <span class="smallcaps">
-      <li><a href="https://github.com/raaghda/millhouseblog">info@millhouse.se</a></li>
-      <li><a href="https://github.com/raaghda/millhouseblog">millhouse.se</a></li>
-      </span>
-    </ul>
-</footer>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-<!-- fontawesome-->
-<script src="https://use.fontawesome.com/67b80e2b65.js"></script>
-<script src="javascript.js"></script> 
-</body>
+        <footer>
+            <ul>
+                <li>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-facebook-square"></i></a>
+                    <a href="#"><i class="fa fa-linkedin-square"></i></a>
+                </li>
+                <span class="smallcaps">
+                    <li>
+                        <a href="https://github.com/raaghda/millhouseblog">info@millhouse.se</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/raaghda/millhouseblog">millhouse.se</a>
+                    </li>
+                </span>
+            </ul>
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+        <!-- fontawesome-->
+        <script src="https://use.fontawesome.com/67b80e2b65.js"></script>
+        <script src="javascript.js"></script> 
+    </body>
 </html>

e483ba41c56a2e8a9d43a45dd6228d43a49633dc
carowood <carowood@gmail.com>
2017-11-27 19:28:20 +0100

    added CKEditor 5 wysiwyg to newpostform and editpost. Added CKEd5 script src to head.php and added CKEd5 styling to CSS

--- www/components/head.php
+++ www/components/head.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <link rel="icon" type="image/png" href="images/favicon_millhouse.png">
+    <script src="https://cdn.ckeditor.com/ckeditor5/1.0.0-alpha.1/classic/ckeditor.js"></script>
 </head>
 <body>
 

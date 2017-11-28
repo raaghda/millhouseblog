a20e38adc8e28ea20cf4ce853b27b6136cfd8db8
patrik909 <patrik.eriksson@medieinstitutet.se>
2017-11-28 20:01:27 +0100

    styled categories

--- www/pages/categories.php
+++ www/pages/categories.php
@@ -1,5 +1,43 @@
-<div class="container categories">
-    <h1 class="heading">Kategorier</h1>   
+<div class="container_categories">
+    <div class="categories_wrapper">
+        <h1 class="light_spacious" id="categories">Kategorier</h1>
+        
+        <div class="image_wrappers">
+        
+        <div class="row">
+        
+        <a href="/millhouseblog/www/?page=category&categoryid=1" id="categoryid1" class="col-sm-12 col-md-6">
+            <h3>SOLGLASÖGON</h3>
+        </a>
+        
+        <a href="/millhouseblog/www/?page=category&categoryid=2" id="categoryid2" class="col-sm-12 col-md-6">
+            <h3>KLOCKOR</h3>
+        </a>
+        
+        </div>
+        
+        <div class="row">
+
+        <a href="/millhouseblog/www/?page=category&categoryid=3" id="categoryid3" class="col-sm-12 col-md-6">
+           
+            <h3>INREDNING</h3>
+        </a>
+        
+        <a href="/millhouseblog/www/?page=category&categoryid=4" id="categoryid4" class="col-sm-12 col-md-6">
+            <h3>LIFESTYLE</h3>
+        </a>
+
+    </div>
+        
+        </div>
+        
+    </div>
+</div>
+  
+
+  
+<!--
+     
 
     <div class="row">
     
@@ -25,7 +63,9 @@
         </a>
 
     </div>
-    
     </div>
-
+    </div>
+    
+    
+-->
    

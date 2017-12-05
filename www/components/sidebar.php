37954883f8f83dc5e180d5d0183efc83a6f3367d
patrik909 <patrik.eriksson@medieinstitutet.se>
2017-12-05 14:57:52 +0100

    still cleaning

--- www/components/sidebar.php
+++ www/components/sidebar.php
@@ -1,6 +1,7 @@
-<?php
-require './parts/database.php';
+<?php /* cleaned */
+    require './parts/database.php';
 
+    /* Fetches the categories*/
 
     $statement = $pdo->prepare(
         "SELECT * FROM category" 
@@ -10,17 +11,15 @@ require './parts/database.php';
 
     $categories = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-$i = 1;
+    $i = 1;
    
-
-$query = "";    
-if(isset($_GET['query'])) {
+    $query = "";    
+        if(isset($_GET['query'])) {
     $query=$_GET['query'];
-}    
-?>
-<div class="sidebar col-12">
-    <aside>
+    }    
+?>   
 
+<aside>
     <div class="aside_search">
         <h3>Sök</h3>
         <form method="get">
@@ -29,87 +28,71 @@ if(isset($_GET['query'])) {
             <input type="submit" value="Sök">
         </form>        
     </div>
-    <div class="aside_category">
-
     <h3>Kategorier</h3>
     <ul class="categories_in_sidebar">
-        
-        <?
-        foreach($categories as $category){ 
+
+        <?php 
+            /* Loops the categorynames */
+            foreach($categories as $category): 
+                
         ?>
-    
-         <li>
-            <a href="/millhouseblog/www/?page=category&categoryid=<?= $i++; ?>">
-            <?= $category["name"];?></a>
+
+        <li>
+            <a href="/millhouseblog/www/?page=category&categoryid=<?= $i++; ?>"><?= $category["name"];?></a>
         </li>
-<? } ?>
+        <? endforeach; ?>
     </ul>
-        </div>
 
-
-    <div class="aside_months">
-    
     <div class="sidebar_underline"></div>
+        
     <h3>Arkiv</h3>
     <ul>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=01">
-            Januari</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=01">Januari</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=02">
-            Februari</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=02">Februari</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=03">
-            Mars</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=03">Mars</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=04">
-            April</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=04">April</a>
+            
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=05">
-            Maj</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=05">Maj</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=06">
-            Juni</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=06">Juni</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=07">
-            Juli</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=07">Juli</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=08">
-            Augusti</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=08">Augusti</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=09">
-            September</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=09">September</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=10">
-            Oktober</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=10">Oktober</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=11">
-            November</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=11">November</a>
         </li>
         <li>
-            <a href="/millhouseblog/www/?page=displaymonth&month=12">
-            December</a>
+            <a href="/millhouseblog/www/?page=displaymonth&month=12">December</a>
         </li>
     </ul>
-          
-          <div class="sidebar_underline"></div>
-           </div>
-           <div class="aside_latest">
 
-            <h3>Aktivitet</h3>
-            <ul class="comments_in_sidebar">
-            <?php //loop out from the latest 5 comments: username and postname. link to that post
-            
+    <div class="sidebar_underline"></div>
+
+    <h3>Aktivitet</h3>
+    <ul class="comments_in_sidebar">
+        <?php 
+            //loop out from the latest 5 comments: username and postname. link to that post
+        
             //loop out from the latest 5 comments: username and postname. link to that post
 
             //fetch comments from database
@@ -120,34 +103,32 @@ if(isset($_GET['query'])) {
 
             for ($i = 0; $i < 8; $i++):
                 if ($i < count($comments)){
-                //store post_id to get post_title
-                //store post_id to be able to link to that specific post
-                $post_id = $comments[$keys[$i]]['postid'];
-
-                //if who made a comment wasnt logged in and therefore has no userid..
-                //..get email from comment table
-                //else store user_id and get username from user table
-                if ($comments[$keys[$i]]['userid'] == NULL){
-                    $username = $comments[$keys[$i]]['email'];
-                } else {
-                    $user_id = $comments[$keys[$i]]['userid'];
-                    $username = get_column_with_input("username", "user", "userid", $user_id);
+                    //store post_id to get post_title
+                    //store post_id to be able to link to that specific post
+                    $post_id = $comments[$keys[$i]]['postid'];
+    
+                    //if who made a comment wasnt logged in and therefore has no userid..
+                    //..get email from comment table
+                    //else store user_id and get username from user table
+                    if ($comments[$keys[$i]]['userid'] == NULL){
+                        $username = $comments[$keys[$i]]['email'];
+                    } else {
+                        $user_id = $comments[$keys[$i]]['userid'];
+                        $username = get_column_with_input("username", "user", "userid", $user_id);
                     }
 
-                //use the stored variables to get info from each table
-                //FUNCTIONS is in function.php
-                $post_title = get_column_with_input("title", "post", "postid", $post_id);
-               
-            ?>
-                <li>
-                    <span class=grey><?= $username ?> kommenterade </span>
-                    <a href="/millhouseblog/www/?page=viewpost&id=<?= $post_id ?>"><?= $post_title ?></a>
-                </li>
+                    //use the stored variables to get info from each table
+                    //FUNCTIONS is in function.php
+                    $post_title = get_column_with_input("title", "post", "postid", $post_id);
 
-        <?php
-        } endfor; ?>
-            </ul>
-            </div>
-        </div>
-        </aside>
-    </div>
+        ?>
+        <li>
+            <span class=grey><?= $username ?> kommenterade </span>
+            <a href="/millhouseblog/www/?page=viewpost&id=<?= $post_id ?>">
+                <?= $post_title ?>
+            </a>
+         </li>
+
+         <?php } endfor; ?>
+    </ul>
+</aside>
--- www/parts/newpostform.php
+++ www/parts/newpostform.php
@@ -1,6 +1,7 @@
-<?php
-require './parts/database.php';
+<?php /*cleaned*/
+    require './parts/database.php';
 
+    /* Fetches the categories*/
 
     $statement = $pdo->prepare(
         "SELECT * FROM category" 
@@ -9,79 +10,68 @@ require './parts/database.php';
     $statement->execute();
 
     $categories = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
-
 ?>
+
 <main class="createpost_page">
-<div class="container createpost">
-   <span class="uppercase">  
-    <h1 class="light_spacious">Skriv nytt inlägg</h1>
-    </span>
-    
-    
-    <?php
-    //if any of the fields not filled, an error message will be returned
-    //using display_notification function from parts/notifyfunctions.php
-    
-    display_notification();
-    
-    ?>
-    
-  
-    <form action="parts/savepost.php" method="POST" enctype="multipart/form-data">
+    <div class="container createpost">
+        <span class="uppercase">  
+            <h1 class="light_spacious">Skriv nytt inlägg</h1>
+        </span>
 
-        <div class="container form">
-            <div class="form-group row">
-                <div class="col-sm-12">
-                   <input type="text" required id="title"  name="title" placeholder=" Titel"> 
+        <?php
+            //if any of the fields not filled, an error message will be returned
+            //using display_notification function from parts/notifyfunctions.php
+
+            display_notification();
+
+        ?>
+
+        <form action="parts/savepost.php" method="POST" enctype="multipart/form-data">
+            <div class="container form">
+                <div class="form-group row">
+                    <div class="col-sm-12">
+                        <input type="text" required id="title"  name="title" placeholder=" Titel"> 
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-12">
+                <div class="form-group row">
+                    <div class="col-sm-12">
                     <!--Textarea using CK Edit 5 ID-->
-                    <textarea name="text" id="editor" rows="10" cols="30"></textarea>
+                        <textarea name="text" id="editor" rows="10" cols="30"></textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-4">
+                        <select required name="categoryid"> 
+                            <option value="" >
+                                Välja Kategori
+                            </option>       
+                            
+                            <?php foreach ($categories as $category): ?>                   
+                            <option value="<?=$category['categoryid'];?>">
+                                <?=$category['name'];?>
+                            </option>   
+                            <?php endforeach; ?>
+                        </select>                    
+                    </div>
+                    <div class="offset-sm-3 col-sm-5">
+                        <input type="file" id="file" class="inputfile" name="file" accept=".jpg, .jpeg, .png, .gif">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-12">
+                    <input id="publish_button" type="submit" name="submit" value="Publicera">
+                    </div>
                 </div>
             </div>
+        </form>
 
-            <div class="form-group row">
-                <div class="col-sm-4">
-                    <select required name="categoryid"> 
-                        <option value="" >Välja Kategori</option>  
-      
-                          <?php
-                            foreach ($categories as $category){
-                            ?>
-                   
-                        <option value="<?=$category['categoryid'];?>"><?=$category['name'];?></option> 
-               
-                          <?php
-                            }
-                            ?>
-                    </select>
-                    
-                </div>
-                <div class="offset-sm-3 col-sm-5">
-                    <input type="file" id="file" class="inputfile" name="file" accept=".jpg, .jpeg, .png, .gif">
-                </div>
-            </div>
-            <div class="form-group row">
-            <div class="col-sm-12">
-                <input id="publish_button" type="submit" name="submit" value="Publicera">
-            </div>
-        </div>
-        </div>
-
-    </form>
-    
- 
-<!-- JS for CK EDITOR 5 -->       
-<script>
-    ClassicEditor
-        .create( document.querySelector( '#editor' ) )
-        .catch( error => {
-            console.error( error );
-        } );
-</script>
-</div>
+        <!-- JS for CK EDITOR 5 -->       
+        <script>
+            ClassicEditor
+            .create( document.querySelector( '#editor' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+        </script>
+    </div>
 </main>

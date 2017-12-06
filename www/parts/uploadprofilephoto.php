e369c1a57511766c08d5f6aef5ff4933f569a82a
raaghda <33297148+raaghda@users.noreply.github.com>
2017-12-06 19:44:57 +0100

    first working version of uploading profilephoto

--- www/parts/uploadprofilephoto.php
+++ www/parts/uploadprofilephoto.php
@@ -1,9 +1,4 @@
 <?php
-var_dump($_POST['submitPhoto']);
-var_dump($_FILES);
-session_start();
-var_dump($_SESSION ['user']['username']);
-
 if(isset($_POST['submitPhoto']) && $_FILES['profilePhoto']['error'] == UPLOAD_ERR_OK){
        
     //created variable equal to superglobal $_FILES and the name of the file ['file']
@@ -30,7 +25,6 @@ if(isset($_POST['submitPhoto']) && $_FILES['profilePhoto']['error'] == UPLOAD_ER
     //variable explode used to separate the name of the file and the extension  
     $fileExt = explode('.', $fileName);
     
-    die("diee");
     //strtolower case function makes the extention lowercase
     //end function gets the last piece of the array, in this case extension
     $fileActualExt = strtolower(end($fileExt));
@@ -41,16 +35,17 @@ if(isset($_POST['submitPhoto']) && $_FILES['profilePhoto']['error'] == UPLOAD_ER
     //boolean in_array checks the file extention ($file_ActualExt) against the types listed in the array   
     if (in_array($fileActualExt, $allowed)){
         if($fileSize < 5000000){
-
             //start uploading the file
             //variable created for new file name in order to avoid overwriting (unique id function using time stamp and file ext)
             $fileNameNew = uniqid('',true).".".$fileActualExt;
 
             //variable created to place file in location 
-            $fileDestination = '../profilepictures/'.$fileNameNew;
-
+            $fileDestination = '../profilephotos/'.$fileNameNew;
+    
             //function movest the file from old temp location  to new location ($fileDestination)
             move_uploaded_file($fileTmpName,$fileDestination);
+            
+            header("Location: /millhouseblog/www/?page=profile");  
 
 
         }else{

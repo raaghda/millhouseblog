4efe3d7eb2529cd329980979413345f288871861
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-07 14:34:14 +0100

    Structure and style-changes for viewpost

--- www/pages/viewpost.php
+++ www/pages/viewpost.php
@@ -11,27 +11,25 @@ display_notification();
 
 ?>
    
-<main class="main_viewpost">
-    <div class="container viewpost">
+    <div class="container-fluid viewpost">
         <div class="row">
-            <div class="col-lg-9">
+            <div class="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-1">
                    
                 <!--INCLUDE PHP FOR DISPLAY POST HERE-->
                 <?php require 'parts/viewpostdisplaypost.php';?>
                       
                 <!--INCLUDE PHP FOR COMMENTS HERE--> 
                 <?php require 'parts/viewpostcomments.php';?>      
-                                    
+             
                 <!--INCLUDE PHP ADD COMMENTS HERE-->
                 <?php require 'parts/viewpostaddcomment.php';?>       
-        
+            
             </div><!--col-lg-9--><!--CONTAINS ALL CONTENT PARTS-->
 
             <!--SIDEBAR-->
-            <div class="col-lg-3 sidebar hidden-xs-down">
+            <div class="col-lg-2 sidebar hidden-xs-down">
                 <?php require 'components/sidebar.php';?>
             </div><!--/sidebar-->
                 
         </div><!--/row-->
     </div><!--end of container viewpost-->
-</main>

01671ce019fcab75933f0b4e7c2be1ba7ab5ce68
Emma Uddesson <emma.uddesson@gmail.com>
2017-12-08 09:56:37 +0100

    Changes the color of "|" separator in month- and category-view

--- www/parts/monthpostcontent.php
+++ www/parts/monthpostcontent.php
@@ -44,7 +44,9 @@
                 <span class="uppercase grey"><?= $username?></span>
                 <p id="post_paragraph"> <?= $post_text; ?> </p>
                 <nav class=””>
-                    <a href="/millhouseblog/www/?page=viewpost&id=<?= $monthpost["postid"]; ?>">Läs hela inlägget</a> | <a href="/millhouseblog/www/?page=viewpost&id=<?= $monthpost["postid"]; ?>#comments">
+                    <a href="/millhouseblog/www/?page=viewpost&id=<?= $monthpost["postid"]; ?>">Läs hela inlägget</a> 
+                    <span class="lightblue"> | </span> 
+                    <a href="/millhouseblog/www/?page=viewpost&id=<?= $monthpost["postid"]; ?>#comments">
                 
                     <!--#comments anchor-->
                     <?= '(' . $number_of_comments . ')'; 
